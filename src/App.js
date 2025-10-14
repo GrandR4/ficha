c46774ed24f218a23713938afa905fcaa827314a
GrandR4 <rodriguezlima1984@gmail.com>
2025-10-14 10:34:21 -0400

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         ¡Hola! Soy Rafael Rodríguez, estudiante de Ingeniería de Sistemas y <br />
+         Estoy dando mis primeros pasos sólidos en React y aquí presento mi portafolio inicial       
+ </p>
+        <a
+          className="App-link"
+          href="https://github.com/GrandR4?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={require('./icono.png')} alt="icono" style={{ width: '20px', verticalAlign: 'middle', marginRight: '8px'}} />Mi Repositorio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
